aba9e27251b9d6098b46726f04e06592aea2fcdc
Suryasg1996 <surya.s@crewfoundry.com>
2021-01-12 19:57:37 +0530

    Made changes building_name validation

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,6 +1,6 @@
 class Building < ApplicationRecord
   has_many :floors, dependent: :destroy
   has_many :rooms, through: :floors
-  validates :name, presence: true, length: { minimum:5 }
+  validates :name, presence: true, length: { minimum:3 }
   validates :number_of_floors, numericality: { only_integer: true }
 end
